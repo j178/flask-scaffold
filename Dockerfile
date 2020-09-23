5882ae4b610ef2ea4f53b59e71cb60039bed8f14
j178 <10510431+j178@users.noreply.github.com>
2020-09-24 05:29:41 +0800

    Add more docker support

--- Dockerfile
+++ Dockerfile
@@ -9,12 +9,5 @@ ENV PYPI "http://mirrors.aliyun.org/pypi/simple"
 WORKDIR /app
 COPY  ./ ./
 
-# 安装 pipenv
-RUN pip install pipenv --no-cache-dir --index ${PYPI} --trusted-host ${PYPI_HOST}
-
-# 使用 pipenv 安装依赖
-RUN set -ex &&  pipenv install --system --deploy
-
-
-# 暴露 gunicorn 监听的 5000 端口，link 容器间可以访问
-EXPOSE 5000
+RUN chmod +x -R ./scripts && scripts/install
+ENTRYPOINT scripts/start
--- config/__init__.py
+++ config/__init__.py
@@ -6,7 +6,7 @@ class BaseConfig:
     SECRET_KEY = b'\x90\xf5\xf5K;\xa5\x85\xacL\x0b\x00\xdf\xb7:,\xc6}\x8b\xc3 9\x10h\xf8\x96\xf1\xb2\xf5\x16\xd1\r\xa0'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://plugin:123456@db:3306/plugins'
+    SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://app:123456@db:3306/app'
 
     # redis 作用：通用缓存、存 session、消息队列 backend
     CACHE_TYPE = 'redis'
