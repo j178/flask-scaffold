9b5d470f3a921ce4307a5530adff48275786dbc7
j178 <10510431+j178@users.noreply.github.com>
2020-09-28 17:01:27 +0800

    Remove old complicated configs

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,56 +1,18 @@
 # Created by John Jiang at 2018/7/6 19:18
 import re
 
-from flask import json
-from flask_sqlalchemy import BaseQuery, SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_sqlalchemy.model import Model as BaseModel
-from sqlalchemy import Column, DateTime, Integer, Text, TypeDecorator, func, text
+from sqlalchemy import Column, DateTime, Integer, func, text
 from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.ext.mutable import MutableDict
-from sqlalchemy.orm import load_only
-from sqlalchemy.orm.base import _generative
-from sqlalchemy.orm.query import _MapperEntity
 
-
-class JSONEncodedDict(TypeDecorator):
-    impl = Text
-
-    def process_bind_param(self, value, dialect):
-        if value is None:
-            return "{}"
-        else:
-            return json.dumps(value)
-
-    def process_result_value(self, value, dialect):
-        if value is None:
-            return {}
-        else:
-            return json.loads(value)
-
-
-MutableDict.associate_with(JSONEncodedDict)
+from app.errors import APIError, errno
 
 session_options = {"expire_on_commit": False}
 
 
-class Query(BaseQuery):
-    @_generative()
-    def only_common_attrs(self):
-        for ent in self._entities:
-            if isinstance(ent, _MapperEntity):
-                class_ = ent.entities[0]
-                if getattr(class_, "common_attrs", None):
-                    # inline query.options()
-                    self._attributes = self._attributes.copy()
-                    if "_unbound_load_dedupes" not in self._attributes:
-                        self._attributes["_unbound_load_dedupes"] = set()
-
-                    opt = load_only(*getattr(class_, "common_attrs"))
-                    self._with_options = self._with_options + (opt,)
-                    opt.process_query(self)
-
-
 class Base(BaseModel):
+    """Base model"""
     common_attrs = ()
     tablename_prefix = ""
 
@@ -76,9 +38,16 @@ class Base(BaseModel):
     )
     deleted_at = Column(DateTime, nullable=True)
 
+    def from_id(self, ident, raise_error=True):
+        """Get instance from primary key"""
+        rv = self.query.get(ident)
+        if rv is None and raise_error:
+            raise APIError(errno.OBJECT_NOT_FOUND, object=self.__class__.__name__)
+        return rv
+
     def to_dict(self):
         # todo
-        self.query.only_common_attrs()
+        return {}
 
 
 # Usage: from app.models import db
@@ -86,21 +55,20 @@ db: SQLAlchemy
 
 
 def init_models(app):
+    """Init db models"""
     global db
 
     Base.tablename_prefix = app.config.get("SQLALCHEMY_TABLE_NAME_PREFIX", "")
     db = SQLAlchemy(
-        model_class=Base, query_class=Query, session_options=session_options
+        model_class=Base, session_options=session_options
     )
 
     db.init_app(app)
     db.app = app
 
-    # reflect_tables(app)
-
     from .models import App, AppUpdate, AppVersion
 
-    # App.create_default_app()
+    App.create_default_app()
     # ...
     # Import more models here
 
@@ -108,7 +76,7 @@ def init_models(app):
     def make_context():
         return dict(
             db=db,
-            query=db.session.query,
+            session=db.session,
             App=App,
             AppVersion=AppVersion,
             AppUpdate=AppUpdate,
